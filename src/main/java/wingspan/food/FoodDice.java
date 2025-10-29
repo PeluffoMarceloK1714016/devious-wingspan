aa0ddff0e429ea37c6532d9cb8c4db9e964715ae
Khare, Rishi- period 5 <K2307266@students.katyisd.org>
2025-10-29 08:23:48 -0500

    first edit

--- src/main/java/wingspan/food/FoodDice.java
+++ src/main/java/wingspan/food/FoodDice.java
@@ -1,5 +1,46 @@
 package main.java.wingspan.food;
 
+import java.awt.image.BufferedImage;
+import java.util.*;
+
 public class FoodDice {
-    
+        private BufferedImage diceFace;
+        private String currentFood;
+
+        private ArrayList<String> foodChoices;
+        private ArrayList<BufferedImage> foodFaces;
+
+        public FoodDice(BufferedImage a, String currentFood){
+            this.diceFace = a;
+            this.currentFood = currentFood;
+            foodChoices = new ArrayList<>();
+            foodChoices.add("invertebrate");
+            foodChoices.add("wheat");
+            foodChoices.add("rodent");
+            foodChoices.add("berry");
+            foodChoices.add("fish");
+
+            foodFaces = new ArrayList<>();
+            foodFaces.add(" file name ");  // corresponds to the indexes of the foodChoices
+            foodFaces.add(" file name ");
+            foodFaces.add(" file name ");
+            foodFaces.add(" file name ");
+            foodFaces.add(" file name ");
+            }
+
+        public void rerollDice(){
+            Random random = new Random();
+            int randomNumber = random.nextInt(6);
+            currentFood = foodChoices.get(randomNumber);
+            diceFace =  foodFaces.get(randomNumber);
+
+        }
+
+        public BufferedImage getImage(){
+            return diceFace;
+        }
+
+        public String getFood(){
+            return currentFood;
+        }
 }
