5b4784477d7ca221c9cd2c77a231db0f94004646
ZhuAndrewK2209615 <K2209615@students.katyisd.org>
2025-10-29 07:59:57 -0500

    Add BonusCard class with image and scoring methods

--- src/main/java/wingspan/cards/BonusCard.java
+++ src/main/java/wingspan/cards/BonusCard.java
@@ -1,5 +1,25 @@
 package main.java.wingspan.cards;
+import java.awt.image.BufferedImage;
 
 public class BonusCard {
-    
+	private BufferedImage cardImage;
+	private int victoryPoints;
+	
+	public BonusCard(BufferedImage image, int points)
+	{
+		cardImage = image;
+		victoryPoints = points;
+	}
+	
+	public BufferedImage getImage()
+	{
+		return cardImage;
+	}
+	
+	public int calculateScore(Player p)
+	{
+		//placeholder; will be overridden in child BonusCard classes
+		return 0;
+	}
 }
+
