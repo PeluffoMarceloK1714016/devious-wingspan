410ff40b89ec5a0f78b3ef934e79d65450253ec5
Peluffo Villalobos <K1714016@students.katyisd.org>
2025-10-30 08:03:27 -0500

    Goal.java created

--- src/main/java/wingspan/cards/goals/Goal.java
+++ src/main/java/wingspan/cards/goals/Goal.java
@@ -1,5 +1,42 @@
 package main.java.wingspan.cards.goals;
+import java.awt.image.BufferedImage;
 
 public class Goal {
-    
+
+    private BufferedImage goalImage; // what the player will be shown
+    private String goalString; // used to identify what the goal actually is
+
+    public Goal(BufferedImage goalImage, String goalString) 
+    {
+        this.goalImage = goalImage;
+        this.goalString = goalString;
+        System.out.println("The Goal " + goalString + " was created.");
+    }
+
+    public int evaluateGoal(Player p)
+    {
+        /*
+         *  
+         * 
+         * if goal met:
+         *      player points + amount
+         *  else:
+         *      no points added (or points deducted depending on whether that is a rule)
+         * 
+         * 
+         */
+
+        return 0; // arbitrary return value for now
+    }
+
+    public BufferedImage getImage()
+    {
+        return this.goalImage;
+    }
+
+    public String getGoalString()
+    {
+        return this.goalString;
+    }
+
 }
