a7c38cdf06ed3d8214b88c36f19f64fbdb7c7290
ZhuAndrewK2209615 <K2209615@students.katyisd.org>
2025-11-02 15:24:07 -0600

    Implement FoodManager with bird feeder functionality

--- src/main/java/wingspan/food/FoodManager.java
+++ src/main/java/wingspan/food/FoodManager.java
@@ -1,5 +1,27 @@
 package main.java.wingspan.food;
+import java.util.*;
+import java.io.*;
 
 public class FoodManager {
-    
+	private ArrayList<FoodDice> birdFeeder;
+	
+	public FoodManager() throws IOException
+	{
+		birdFeeder = new ArrayList<FoodDice>();
+		reroll();
+	}
+	
+	public void reroll() throws IOException
+	{
+		birdFeeder.clear();
+		for(int i=0; i<5; i++)
+		{
+			birdFeeder.add(new FoodDice());
+		}
+	}
+	
+	public FoodDice getDie(int index)
+	{
+		return birdFeeder.get(index);
+	}
 }
