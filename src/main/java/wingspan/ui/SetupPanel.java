acf2c4e4fc3918fd7bf7254b3c12dd015f64857e
ZhangAlexK1341401 <alexdeboiiii@gmail.com>
2025-11-03 08:14:52 -0600

    Update SetupPanel.java

--- src/main/java/wingspan/ui/SetupPanel.java
+++ src/main/java/wingspan/ui/SetupPanel.java
@@ -1,5 +1,36 @@
 package main.java.wingspan.ui;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.*;
+import java.io.*;
 
-public class SetupPanel{
+public class SetupPanel extends JPanel implements KeyListener, MouseListener{
 
+    public SetupPanel() {
+		addMouseListener(this);
+		addKeyListener(this);
+	}
+    
+    public void addNotify(){
+        super.addNotify();
+        requestFocus();
+    }
+
+    public void paint(Graphics g) {
+    	super.paint(g);
+    }
+    
+    addKeyListener(this);
+    public void keyPressed(KeyEvent e){}
+    public void keyReleased(KeyEvent e){}
+    public void keyTyped(KeyEvent e){}
+
+    public void mouseClicked(MouseEvent e) {}
+	public void mousePressed(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
 }
