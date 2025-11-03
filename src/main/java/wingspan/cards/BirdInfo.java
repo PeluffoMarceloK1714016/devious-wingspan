c6fcf69c36405e539b96d07fa37d1f2c25cb96b2
Marcelo Peluffo Villalobos <k1714016@students.katyisd.org>
2025-11-03 18:22:20 +0000

    moved around methods

--- src/main/java/wingspan/cards/BirdInfo.java
+++ src/main/java/wingspan/cards/BirdInfo.java
@@ -19,4 +19,72 @@ public class BirdInfo {
         // etc...
         // behavior = new (Type)Behavior; which is determined when hard coding bird info
     }
+
+     public boolean addEggs(int amount)
+    {
+        if (currentEggs + amount <= maxEggs)
+        {
+            currentEggs += amount;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public boolean hasHabitat(String s)
+    {
+        return habitats.contains(s);
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getPoints()
+    {
+        return victoryPoints;
+    }
+
+    public BufferedImage getImage()
+    {
+        return cardImage;
+    }
+
+    public void addTuckedCard(Card c)
+    {
+        tuckedCards.add(c);
+    }
+
+    public void addFoodtoken(String s)
+    {
+        foodTokens.add(s);
+    }
+
+    public int getVictoryPoints() 
+    {
+        return victoryPoints;
+    }
+
+    public void setVictoryPoints(int victoryPoints) 
+    {
+        this.victoryPoints = victoryPoints;
+    }
+
+    public void setMaxEggs(int maxEggs) 
+    {
+        this.maxEggs = maxEggs;
+    }
+
+    public ArrayList<Card> getTuckedCards() 
+    {
+        return tuckedCards;
+    }
+
+    public ArrayList<String> getFoodTokens() 
+    {
+        return foodTokens;
+    }
 }
