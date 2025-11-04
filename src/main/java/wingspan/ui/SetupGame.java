326bcdf22418925d193b39b4cd84c650780850cc
ZhangAlexK1341401 <alexdeboiiii@gmail.com>
2025-11-04 07:44:29 -0600

    Add variables to SetupGame.java

--- src/main/java/wingspan/ui/SetupGame.java
+++ src/main/java/wingspan/ui/SetupGame.java
@@ -1,5 +1,9 @@
 package wingspan.ui;
 
 public class SetupGame {
+    boolean isSetup = true;
     
+    int currentPlayer = 1;
+    boolean playerIsSet = false; //This boolean will signal whether or not the current player is has completed the set up stage.
+        //Once the current player completes the set up stage, it will switch to true, display the set up stage for the next player, and then change to false.
 }
