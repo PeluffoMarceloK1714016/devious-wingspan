eda474408a2a155abc1d073d665a38492acf248b
PeluffoMarceloK1714016 <187894943+MarcePV@users.noreply.github.com>
2025-11-05 21:38:44 -0600

    idea with enums

--- src/main/java/wingspan/core/GameState.java
+++ src/main/java/wingspan/core/GameState.java
@@ -1,5 +1,35 @@
 package wingspan.core;
 
+enum Input {
+
+	CIRCLE_1,
+	CIRCLE_2,
+	CIRCLE_3,
+	CIRCLE_4,
+	CIRCLE_5,
+	CARD_1,
+	CARD_2,
+	CARD_3,
+	CARD_4,
+	CARD_5,
+
+}
+
+
 public class GameState {
-    
+	
+
+
+	
+	public void addPlayerSelection(Input in)
+	{
+		switch (in)
+		{
+			case CIRCLE_1 -> System.out.println("Circle 1 Selected.");
+			default:
+				System.out.println("Nevermind.");
+				//this is the idea
+		}
+	}
+
 }
