f4f88b2dd1d2b7078cc2dbdd138ebf56a948fe57
ZhuAndrewK2209615 <K2209615@students.katyisd.org>
2025-11-06 07:26:11 -0600

    Initialize game management variables in GameState
    Added imports and static variable declarations for game management.

--- src/main/java/wingspan/core/GameState.java
+++ src/main/java/wingspan/core/GameState.java
@@ -1,5 +1,10 @@
 package wingspan.core;
+import java.util.*;
 
 public class GameState {
-    
+	//these are initialized in WingspanRunner's main method
+	public static FoodManager foodManager;
+	public static CardManager cardManager;
+	public static ArrayList<Player> players;
+	public static GoalBoard goalBoard;
 }
