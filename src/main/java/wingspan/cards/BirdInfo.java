2bb4eb4d4ad6a2655426132ee5e7da9ab8103840
ZiqingOliviaL <K2213710@students.katyisd.org>
2025-11-06 07:35:15 -0600

    Refactor BirdInfo class and update foodCost type

--- src/main/java/wingspan/cards/BirdInfo.java
+++ src/main/java/wingspan/cards/BirdInfo.java
@@ -1,81 +1,39 @@
 package wingspan.cards;
-
 import wingspan.cards.behavior.*;
+
 import java.util.*;
 
 public class BirdInfo {
-    private final String name;
-    private final HashSet<String> habitats;
-    private final int victoryPoints;
-    private final String[] foodCost;
-    private final String nestType;
-    private final int maxEggs;
-    private final int wingspan;
-    private final String powerColor;
+    private final String name; // Bird name
+    private final HashSet<String> habitats; // Habitats: forest, grassland, wetland
+    private final String[][] foodCost; // Food cost: fish, invertebrate, grain, fruit, rodent, wild
+    private final int victoryPoints; // Victory points
+    private final String nestType; // Nest type: bowl, cavity, platform, ground, star, none
+    private final int maxEggs; // Maximum number of eggs
+    private final int wingSpan; // Wingspan in cm
+    private final String powerColor; // Power color: brown, pink, white, none
     private final PowerBehavior behavior;
 
-    public BirdInfo(String name, HashSet<String> habitats, int victoryPoints, String[] foodCost, String nestType, int maxEggs, int wingspan, String powerColor, PowerBehavior behavior)
-    {
+    public BirdInfo(String name, HashSet<String> habitats, String[][] foodCost, int victoryPoints,
+                    String nestType, int maxEggs, int wingSpan, String powerColor, PowerBehavior behavior) {
         this.name = name;
         this.habitats = habitats;
-        this.victoryPoints = victoryPoints;
         this.foodCost = foodCost;
+        this.victoryPoints = victoryPoints;
         this.nestType = nestType;
         this.maxEggs = maxEggs;
-        this.wingspan = wingspan;
+        this.wingSpan = wingSpan;
         this.powerColor = powerColor;
         this.behavior = behavior;
     }
 
-
-
-    public boolean hasHabitat(String s)
-    {
-        return habitats.contains(s);
-    }
-
-    public String getName()
-    {
-        return name;
-    }
-
-    public int getPoints()
-    {
-        return victoryPoints;
-    }
-
-    public int getVictoryPoints() 
-    {
-        return victoryPoints;
-    }
-
-    public String[] getFoodCost() 
-    {
-        return foodCost;
-    }
-
-    public String getNestType() 
-    {
-        return nestType;
-    }
-
-    public int getMaxEggs() 
-    {
-        return maxEggs;
-    }
-
-    public int getWingspan() 
-    {
-        return wingspan;
-    }
-
-    public String getPowerColor()
-    {
-        return powerColor;
-    }
-
-    public PowerBehavior getBehavior() 
-    {
-        return behavior;
-    }
+    public String getName() { return name; }
+    public HashSet<String> getHabitats() { return habitats; }
+    public String[][] getFoodCost() { return foodCost; }
+    public int getVictoryPoints() { return victoryPoints; }
+    public String getNestType() { return nestType; }
+    public int getMaxEggs() { return maxEggs; }
+    public int getWingSpan() { return wingSpan; }
+    public String getPowerColor() { return powerColor; }
+    public PowerBehavior getBehavior() { return behavior; }
 }
