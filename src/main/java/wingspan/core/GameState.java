a9c5cc9a22e738d7909871f361b7ff75e2323e4e
PeluffoMarceloK1714016 <K1714016@students.katyisd.org>
2025-11-06 07:32:24 -0600

    Fixed imports

--- src/main/java/wingspan/core/GameState.java
+++ src/main/java/wingspan/core/GameState.java
@@ -1,6 +1,9 @@
 package wingspan.core;
 import java.util.*;
 
+import wingspan.food.FoodManager;
+import wingspan.cards.CardManager;
+
 public class GameState {
 	//these are initialized in WingspanRunner's main method
 	public static FoodManager foodManager;
