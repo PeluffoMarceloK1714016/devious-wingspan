9e3c4118a2bab4d44867cfd6b8e788ccb08fc859
ZiqingOliviaL <K2213710@students.katyisd.org>
2025-11-06 18:11:37 -0600

    Update TuckCardBehavior.java

--- src/main/java/wingspan/cards/behavior/TuckCardBehavior.java
+++ src/main/java/wingspan/cards/behavior/TuckCardBehavior.java
@@ -1,5 +1,32 @@
 package wingspan.cards.behavior;
 
-public class TuckCardBehavior {
-    
+public class TuckCardBehavior implements PowerBehavior {
+    private int numCards;
+    private boolean fromHand;
+
+    // Constructor 1: tuck X cards from hand
+    public TuckCardBehavior(int numCards) {
+        this.numCards = numCards;
+        this.fromHand = true; // default: tuck from hand
+    }
+
+    // Constructor 2: specify whether the card comes from hand or deck
+    public TuckCardBehavior(int numCards, boolean fromHand) {
+        this.numCards = numCards;
+        this.fromHand = fromHand;
+    }
+
+    @Override
+    public boolean executePower() {
+        // tuck card behavior
+        return true;
+    }
+
+    public int getNumCards() {
+        return numCards;
+    }
+
+    public boolean isFromHand() {
+        return fromHand;
+    }
 }
