756bb39192a31ea54c9f478ac1840852991ddaa6
ZiqingOliviaL <K2213710@students.katyisd.org>
2025-11-06 18:05:33 -0600

    Rename and simplify PlayPreviousBrownBehavior class
    Refactored PlayPreviousBrownBehavior class to PlayPreviousBrown and simplified power activation logic.

--- src/main/java/wingspan/cards/behavior/PlayPreviousBrown.java
+++ src/main/java/wingspan/cards/behavior/PlayPreviousBrown.java
@@ -1,32 +1,15 @@
 package wingspan.cards.behavior;
 
-public class PlayPreviousBrownBehavior implements PowerBehavior {
-    private int numPowers;
-    private boolean activateAll;
-
-    // Constructor 1: default: activate 1 previous brown power
-    public PlayPreviousBrownBehavior(int numPowers) {
-        this.numPowers = numPowers;
-        this.activateAll = false;   // default: only the nearest previous brown
-    }
-
-    // Constructor 2: choose whether to activate ALL previous brown powers
-    public PlayPreviousBrownBehavior(int numPowers, boolean activateAll) {
-        this.numPowers = numPowers;
-        this.activateAll = activateAll;
-    }
+public class PlayPreviousBrown implements PowerBehavior {
+    boolean playedBrownBefore = true; // sample
 
     @Override
     public boolean executePower() {
-        // play previous brown power behavior
+        if (playedBrownBefore) {
+            // play previous brown
+        }
+
         return true;
     }
 
-    public int getNumPowers() {
-        return numPowers;
-    }
-
-    public boolean activateAll() {
-        return activateAll;
-    }
 }
