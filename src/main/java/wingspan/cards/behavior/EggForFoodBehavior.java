247c0afd8fd741eb56acb1bb94409a5cdbfa9d50
ZiqingOliviaL <K2213710@students.katyisd.org>
2025-11-07 07:49:36 -0600

    Add onThisBird parameter to EggForFoodBehavior constructors

--- src/main/java/wingspan/cards/behavior/EggForFoodBehavior.java
+++ src/main/java/wingspan/cards/behavior/EggForFoodBehavior.java
@@ -3,19 +3,22 @@ package wingspan.cards.behavior;
 public class EggForFoodBehavior implements PowerBehavior {
     private int numEggs;
     private int numFood;
+    private boolean onThisBird;
     private String typeOFFood;
 
     // Constructor 1
-    public EggForFoodBehavior(int numEggs, int numFood, String typeOFFood) {
+    public EggForFoodBehavior(int numEggs, int numFood, boolean onThisBird, String typeOFFood) {
         this.numEggs = numEggs;
         this.numFood = numFood;
+        this.onThisBird = onThisBird;
         this.typeOFFood = typeOFFood;
     }
 
     // Constructor 2 (no food type given)
-    public EggForFoodBehavior(int numEggs, int numFood) {
+    public EggForFoodBehavior(int numEggs, int numFood, boolean onThisBird) {
         this.numEggs = numEggs;
         this.numFood = numFood;
+        this.onThisBird = onThisBird;
         this.typeOFFood = "any";
     }
 
@@ -31,6 +34,10 @@ public class EggForFoodBehavior implements PowerBehavior {
 
     public int getNumFood() {
         return numFood;
+    }
+
+    public boolean getOnThisBird() {
+        return onThisBird;
     }
 
     public String getTypeOFFood() {
