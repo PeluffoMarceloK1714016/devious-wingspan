1134391757a866946999acdfe2ff305ea57406da
PeluffoMarceloK1714016 <187894943+MarcePV@users.noreply.github.com>
2025-11-06 20:00:43 -0600

    get & set

--- src/main/java/wingspan/core/GameState.java
+++ src/main/java/wingspan/core/GameState.java
@@ -6,8 +6,62 @@ import wingspan.cards.CardManager;
 
 public class GameState {
 	//these are initialized in WingspanRunner's main method
-	public static FoodManager foodManager;
-	public static CardManager cardManager;
-	public static ArrayList<Player> players;
-	public static GoalBoard goalBoard;
+	private FoodManager foodManager;
+	private CardManager cardManager;
+	private ArrayList<Player> players;
+	private Player activePlayer;
+	private GoalBoard goalBoard;
+
+	public FoodManager getFoodManager()
+	{
+		return foodManager;
+	}
+
+	public void setFoodManager(FoodManager foodManager)
+	{
+		this.foodManager = foodManager;
+	}
+
+	public CardManager getCardManager()
+	{
+		return cardManager;
+	}
+
+	public void setCardManager(CardManager cardManager)
+	{
+		this.cardManager = cardManager;
+	}
+
+	public ArrayList<Player> getPlayers()
+	{
+		return players;
+	}
+
+	public void setPlayers(ArrayList<Player> players)
+	{
+		this.players = players;
+	}
+
+	public Player getActivePlayer()
+	{
+		return activePlayer;
+	}
+
+	public void setActivePlayer(Player activePlayer)
+	{
+		this.activePlayer = activePlayer;
+	}
+
+	public GoalBoard getGoalBoard()
+	{
+		return goalBoard;
+	}
+
+	public void setGoalBoard(GoalBoard goalBoard)
+	{
+		this.goalBoard = goalBoard;
+	}
+
+	
+
 }
