bb24a5f7ab4bbe5be1db91fefc4472dfddb9703a
ZiqingOliviaL <K2213710@students.katyisd.org>
2025-11-07 07:41:18 -0600

    Add constructor for second PowerBehavior in TuckCardBehavior
    Added a new constructor to TuckCardBehavior to allow specifying a second PowerBehavior.

--- src/main/java/wingspan/cards/behavior/TuckCardBehavior.java
+++ src/main/java/wingspan/cards/behavior/TuckCardBehavior.java
@@ -3,6 +3,7 @@ package wingspan.cards.behavior;
 public class TuckCardBehavior implements PowerBehavior {
     private int numCards;
     private boolean fromHand;
+    private PowerBehavior secondBehavior;
 
     // Constructor 1: tuck X cards from hand
     public TuckCardBehavior(int numCards) {
@@ -14,6 +15,13 @@ public class TuckCardBehavior implements PowerBehavior {
     public TuckCardBehavior(int numCards, boolean fromHand) {
         this.numCards = numCards;
         this.fromHand = fromHand;
+    }
+
+    // Constructor 3: specify whether the card comes from hand or deck + second behavior
+    public TuckCardBehavior(int numCards, boolean fromHand, PowerBehavior secondBehavior) {
+        this.numCards = numCards;
+        this.fromHand = fromHand;
+        this.secondBehavior = secondBehavior;
     }
 
     @Override
