16e199123d996b1a9a422a9316c79372ec56a1f3
PeluffoMarceloK1714016 <K1714016@students.katyisd.org>
2025-11-07 07:34:29 -0600

    revert to static

--- src/main/java/wingspan/core/GameState.java
+++ src/main/java/wingspan/core/GameState.java
@@ -1,67 +1,13 @@
 package wingspan.core;
-import java.util.*;
 
-import wingspan.food.FoodManager;
-import wingspan.cards.CardManager;
+import java.util.*;
+import wingspan.food.*;
+import wingspan.cards.*;
 
 public class GameState {
 	//these are initialized in WingspanRunner's main method
-	private FoodManager foodManager;
-	private CardManager cardManager;
-	private ArrayList<Player> players;
-	private Player activePlayer;
-	private GoalBoard goalBoard;
-
-	public FoodManager getFoodManager()
-	{
-		return foodManager;
-	}
-
-	public void setFoodManager(FoodManager foodManager)
-	{
-		this.foodManager = foodManager;
-	}
-
-	public CardManager getCardManager()
-	{
-		return cardManager;
-	}
-
-	public void setCardManager(CardManager cardManager)
-	{
-		this.cardManager = cardManager;
-	}
-
-	public ArrayList<Player> getPlayers()
-	{
-		return players;
-	}
-
-	public void setPlayers(ArrayList<Player> players)
-	{
-		this.players = players;
-	}
-
-	public Player getActivePlayer()
-	{
-		return activePlayer;
-	}
-
-	public void setActivePlayer(Player activePlayer)
-	{
-		this.activePlayer = activePlayer;
-	}
-
-	public GoalBoard getGoalBoard()
-	{
-		return goalBoard;
-	}
-
-	public void setGoalBoard(GoalBoard goalBoard)
-	{
-		this.goalBoard = goalBoard;
-	}
-
-	
-
+	public static FoodManager foodManager;
+	public static CardManager cardManager;
+	public static ArrayList<Player> players;
+	public static GoalBoard goalBoard;
 }
