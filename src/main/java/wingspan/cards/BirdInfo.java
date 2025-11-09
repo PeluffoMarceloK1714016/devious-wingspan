f54c174694b1c9c1e4abdf935379285df3a6cbdc
MarcePV <marcepv1@outlook.com>
2025-11-08 22:49:37 -0600

    Updated variables with enums instead

--- src/main/java/wingspan/cards/BirdInfo.java
+++ src/main/java/wingspan/cards/BirdInfo.java
@@ -1,21 +1,22 @@
 package wingspan.cards;
 import wingspan.cards.behavior.*;
+import wingspan.enums.*;
 
 import java.util.*;
 
 public class BirdInfo {
     private final String name; // Bird name
-    private final HashSet<String> habitats; // Habitats: forest, grassland, wetland
-    private final String[][] foodCost; // Food cost: fish, invertebrate, grain, fruit, rodent, wild
+    private final EnumSet<Habitat> habitats; // Habitats: forest, grassland, wetland
+    private final Food[][] foodCost; // Food cost: fish, invertebrate, grain, fruit, rodent, wild
     private final int victoryPoints; // Victory points
-    private final String nestType; // Nest type: bowl, cavity, platform, ground, star, none
+    private final NestType nestType; // Nest type: bowl, cavity, platform, ground, star, none
     private final int maxEggs; // Maximum number of eggs
     private final int wingSpan; // Wingspan in cm
-    private final String powerColor; // Power color: brown, pink, white, none
+    private final Color powerColor; // Power color: brown, pink, white, none
     private final PowerBehavior behavior;
 
-    public BirdInfo(String name, HashSet<String> habitats, String[][] foodCost, int victoryPoints,
-                    String nestType, int maxEggs, int wingSpan, String powerColor, PowerBehavior behavior) {
+    public BirdInfo(String name, EnumSet<Habitat> habitats, Food[][] foodCost, int victoryPoints,
+                    NestType nestType, int maxEggs, int wingSpan, Color powerColor, PowerBehavior behavior) {
         this.name = name;
         this.habitats = habitats;
         this.foodCost = foodCost;
@@ -28,12 +29,12 @@ public class BirdInfo {
     }
 
     public String getName() { return name; }
-    public HashSet<String> getHabitats() { return habitats; }
-    public String[][] getFoodCost() { return foodCost; }
+    public EnumSet<Habitat> getHabitats() { return habitats; }
+    public Food[][] getFoodCost() { return foodCost; }
     public int getVictoryPoints() { return victoryPoints; }
-    public String getNestType() { return nestType; }
+    public NestType getNestType() { return nestType; }
     public int getMaxEggs() { return maxEggs; }
     public int getWingSpan() { return wingSpan; }
-    public String getPowerColor() { return powerColor; }
+    public Color getPowerColor() { return powerColor; }
     public PowerBehavior getBehavior() { return behavior; }
 }
